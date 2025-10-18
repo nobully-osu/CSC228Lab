6992232f80ebd1949405d4a4e996ec4ffb13ff0d
nobully-osu <matthew-313@live.com>
2025-10-17 22:19:02 -0500

    Update CombinationLock.v

--- Module7/7.15/CombinationLock.v
+++ Module7/7.15/CombinationLock.v
@@ -7,25 +7,51 @@ module CombinationLock (
 );
 
 parameter S0 = 3'b000, S1 = 3'b001, S2 = 3'b010, S3 = 3'b011, S4 = 3'b100, S5 = 3'b101;
+reg [2:0] state_next;
 
 initial state = S0;
   always @(rst,zero,one) begin
-    unlocked = 0;
-
-    if (rst)
-      state = S0;
-    else begin
+    unlocked   = 0;
+    state_next = state;
+    
+    if (rst) begin
+      state_next = S0;
+      unlocked   = 0;
+    end else begin
       case (state)
-        S0: state = (zero) ? S1 : S0;
-        S1: state = (one)  ? S2 : (zero ? S1 : S0);
-        S2: state = (zero) ? S3 : (one ? S2 : S0);
-        S3: state = (one)  ? S4 : (zero ? S1 : S0);
-        S4: state = (one)  ? S5 : (zero ? S1 : S0);
+        S0: begin 
+          if (zero) state_next = S1; 
+          else state_next = S0;
+        end
+        S1: begin 
+          if (one) state_next = S2; 
+          else if (zero) state_next = S1;
+          else state_next = S1;
+        end
+        S2: begin 
+          if (zero) state_next = S3; 
+          else if (one) state_next = S0;
+          else state_next = S2;
+        end
+        S3: begin 
+          if (one) state_next = S4; 
+          else if (zero) state_next = S1;
+          else state_next = S3;
+        end
+        S4: begin 
+          if (one) state_next = S5; 
+          else if (zero) state_next = S3;
+          else state_next = S4;
+        end
         S5: begin
-          unlocked = 1;
-          state = (zero) ? S1 : (one ? S2 : S0);
+          if (zero) state_next = S1; 
+          else if (one) state_next = S0;
+          else state_next = S5;
         end
       endcase
     end
+
+    state    = state_next; // update state at end of loop
+    unlocked = (state == S5) ? 1 : 0; // check if state is S5 to unlock, otherwise leave locked
   end
 endmodule
