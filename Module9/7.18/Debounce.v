ff6822665b91e8f0a952429813ef78d6333f1094
nobully_osu <matthew-313@live.com>
2025-10-29 16:03:00 -0500

    Update Debounce.v

--- Module9/7.18/Debounce.v
+++ Module9/7.18/Debounce.v
@@ -1,48 +1,33 @@
-module Debounce ( // edge detect, synchronize to clock, debounce input pushbutton or switch
+module Debounce (  // edge detect, synchronize to clock, debounce input pushbutton or switch
     input clk,
-    b,            // b is the bouncy pushbutton or switch
-    output reg s  // debounced version of pushbutton, synched to clock.
-                  // ASSUMES the bouncing continues for less than 2 clock periods  
+    b,  // b is the bouncy pushbutton or switch
+    output reg s     // debounced version of pushbutton, synched to clock.
+                     // ASSUMES the bouncing continues for less than 2 clock periods  
 );
-  reg WaitRise, SkipRise, WaitFall, SkipFall;
-  reg b_sync_0, b_sync_1;
+  reg [1:0] currentState;
+  parameter WaitRise = 2'b00, SkipRise = 2'b01, WaitFall = 2'b10, SkipFall = 2'b11;
 
   initial begin
-    b_sync_0 = 0;
-    b_sync_1 = 0;
     s = 0;
-    WaitRise = 1;
-    SkipRise = 0;
-    WaitFall = 0;
-    SkipFall = 0;
+    currentState = WaitRise;
   end
 
   always @(posedge clk) begin
-    b_sync_0 <= b;
-    b_sync_1 <= b_sync_0;
-  end
-
-  wire rising  = (b_sync_0 & ~b_sync_1);
-  wire falling = (~b_sync_0 & b_sync_1);
-
-  always @(posedge clk) begin
-    if (WaitRise && rising) begin
-      s <= 1;
-      WaitRise <= 0;
-      SkipRise <= 1;
-      WaitFall <= 1;
-      SkipFall <= 0;
-    end
-    else if (SkipRise && rising) begin
-    end
-    else if (WaitFall && falling) begin
-      s <= 0;
-      WaitFall <= 0;
-      WaitRise <= 1;
-      SkipFall <= 1; 
-      SkipRise <= 0;
-    end
-    else if (SkipFall && falling) begin
-    end 
+    case (currentState)
+      WaitRise: begin
+        if (b) begin
+          s <= 1;
+          currentState <= SkipRise;
+        end else currentState <= WaitRise;
+      end
+      SkipRise: currentState <= WaitFall;
+      WaitFall: begin
+        if (~b) begin
+          s <= 0;
+          currentState <= SkipFall;
+        end else currentState <= WaitFall;
+      end
+      SkipFall: currentState <= WaitRise;
+    endcase
   end
 endmodule
